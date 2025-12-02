960248e91bb0719a7486c0c8c0978d8389d67a08
luke <lfairbanks@anamosa.k12.ia.us>
2025-12-02 19:21:12 +0000

    done

--- index.js
+++ index.js
@@ -1,19 +1,19 @@
 // Declare a function named `square` that returns the number multiplied by itself
 function square(n) {
-	return n * n;
-
+  return n * n;
+}
 
 // If running in a browser, attach `square` to the global `window` object
 if (typeof window !== 'undefined') {
-	window.square = square;
+  window.square = square;
 
-	// Set the innerHTML of the element with id `result` to the square of 5
-	const el = document.getElementById('result');
-	if (el) el.innerHTML = String(square(5));
+  // Set the innerHTML of the element with id `result` to the square of 5
+  const el = document.getElementById('result');
+  if (el) el.innerHTML = String(square(5));
 }
 
 // Export for CommonJS environment / tests running in Node
 if (typeof module !== 'undefined' && module.exports) {
-	module.exports.square = square;
+  module.exports.square = square;
 }
 
